84c36b64c68daa22ff89295cfc45449789014082
ltetrel <loic.tetrel.pro@gmail.com>
2021-05-26 14:34:05 -0400

    updating tag for version 2.3

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(name='repo2data',
-      version='2.2',
+      version='2.3',
       description='Automatic data fetcher from a remote server.',
       url='https://github.com/SIMEXP/Repo2Data',
-      download_url='https://github.com/SIMEXP/Repo2Data/archive/2.2.tar.gz',
+      download_url='https://github.com/SIMEXP/Repo2Data/archive/2.3.tar.gz',
       author='Loic TETREL',
       author_email='loic.tetrel.pro@gmail.com',
       license='MIT',
